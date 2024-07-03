5caf769c319ba8885b8f0611dec99830a66028a2
Linus Broms <lbroms@c1r1p10.hive.fi>
2024-07-03 19:36:41 +0300

    Flow: Add models used for user management

--- flow_container/flow/api/models.py
+++ flow_container/flow/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	avatar = models.ImageField(upload_to='api/static/avatars/')
+	friendList = models.ManyToManyField('self', blank=True)
+	blockedList = models.ManyToManyField('self', blank=True)
+	matches = models.ManyToManyField('Match', blank=True)
+	sentMessages = models.ManyToManyField('Message', blank=True, related_name='sent_messages')
+	receivedMessages = models.ManyToManyField('Message', blank=True, related_name='received_messages')
+
+	def __str__(self):
+		return self.user.username
+
+class Match(models.Model):
+	matchId = models.IntegerField()
+	matchName = models.CharField(max_length=50)
+	matchDate = models.DateTimeField()
+	matchWinner = models.ForeignKey(Account, on_delete=models.SET_NULL, blank=False, null=True, related_name='match_winner')
+	matchLoser = models.ForeignKey(Account, on_delete=models.SET_NULL, blank=False, null=True, related_name='match_loser')
+	matchWinnerScore = models.IntegerField()
+	matchLoserScore = models.IntegerField()
+
+class Message(models.Model):
+	messageContent = models.CharField(max_length=500)
+	messageSender = models.ForeignKey(Account, on_delete=models.CASCADE, blank=False, null=False, related_name='message_sender')
+	messageReceiver = models.ForeignKey(Account, on_delete=models.CASCADE, blank=False, null=False, related_name='message_receiver')
+	messageDate = models.DateTimeField(null=True, blank=True)
+
