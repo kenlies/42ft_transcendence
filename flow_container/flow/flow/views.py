7a507f87b38f484d20540d7685e86068827a5f08
Linus Broms <lbroms@c1r1p10.hive.fi>
2024-07-03 19:28:02 +0300

    Flow: Add index serving view and dependencies

---
+++ flow_container/flow/flow/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+@ensure_csrf_cookie
+def index(request):
+	template = loader.get_template('pong/index.html')
+	return HttpResponse(template.render())
