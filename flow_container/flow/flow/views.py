51343941e57fe2da7a0195e381772476fe31fa92
Joonas Henriksson <joonas.henriksson@gmail.com>
2024-07-24 18:22:51 +0300

    flow: Load stylesheet from django template
    Mainly this allows splitting the stylesheet into multiple files,
    while still linking only one CSS file in the HTML. This should be
    way cleaner and easier to read than one huge CSS file, and easier
    to manage than linkin multiple CSS files in HTML. Additionally we
    can use django template functionalities in the CSS, which could be
    very useful.

--- flow_container/flow/flow/views.py
+++ flow_container/flow/flow/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.shortcuts import render
 
 @ensure_csrf_cookie
 def index(request):
 	template = loader.get_template('pong/index.html')
 	return HttpResponse(template.render())
+
+@ensure_csrf_cookie
+def stylesheet(request):
+	return render(request, 'css/styles.css', {}, content_type="text/css")
