ec741b9fb1cebba3d45d8ebbb00ce175b98f83e2
Joonas Henriksson <joonas.henriksson@gmail.com>
2024-08-01 02:33:47 +0300

    pong.js: Add function to set up click events for page links
    The function adds click event listener links that navigate the
    application to another sub-page. Those links will be identified from
    'page_link' class.
    
    Clicking will get the content from the href-attribute of the link,
    and the attribute should be prepended with '#' to avoid accidentally
    linking to another (broken) page.
    
    For example, clicking the following gets the page from /content/page:
    <a class="page_link" href="#page">link to page</a>

--- flow_container/flow/flow/static/js/pong.js
+++ flow_container/flow/flow/static/js/pong.js
@@ -2,3 +2,16 @@ const changeContainerContent = async (container, contentUrl) => {
     const response = await fetch('/content/' + contentUrl);
     container.innerHTML = await response.text();
 }
+
+const initPageLinks = () => {
+    const pageLinks = document.getElementsByClassName('page_link');
+    
+    for (let i = 0; i < pageLinks.length; i++) {
+        pageLinks[i].addEventListener('click',
+            (event) => {
+                event.preventDefault();
+                changeContainerContent(document.body, pageLinks[i].getAttribute('href').substr(1));
+            }
+        );
+    }
+}
