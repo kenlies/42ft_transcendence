e765af18955acfd721bd84980d45c1ef03e6206a
alexander <alexander.otsala@gmail.com>
2024-08-05 00:12:22 +0300

    Flow_container: use daphne instead of uvicorn
    and add some pesky new lines

--- flow_container/entry.sh
+++ flow_container/entry.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
 
-exec "uvicorn" "flow.asgi:application" "--host" "0.0.0.0"
+exec "daphne" "-b" "0.0.0.0" "-p" "8000" "flow.asgi:application"
