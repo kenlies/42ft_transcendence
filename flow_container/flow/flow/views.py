fa0a4af73f8374c4b668d0aa2dd156f2b21d13ac
Dardan <dardangerguri@outlook.com>
2024-08-15 20:42:30 +0300

    flow/views.py: Add support for user and target user profile in content view
    Updated the content endpoint to accept an optional targetUsername parameter.
    Modified the logic to get the users details, to be able to display them.
    The blocked users messages are not included in the context.

--- flow_container/flow/flow/views.py
+++ flow_container/flow/flow/views.py
@@ -18,7 +18,7 @@ def javascript(request):
 	return render(request, 'js/scripts.js', {}, content_type="text/javascript")
 
 @ensure_csrf_cookie
-def content(request, content):
+def content(request, content, targetUsername=None):
 	no_allow = [
 		'index',
 	]
@@ -31,9 +31,46 @@ def content(request, content):
 		context = {}
 		if (content in no_auth or request.user.is_authenticated) and content not in no_allow:
 			try:
-				context['user'] = Account.objects.get(user=request.user)
+				user = Account.objects.get(user=request.user)
+				context['user'] = user
+				context['blocked'] = user.blockedList.all()
+				context['friends'] = user.friendList.exclude(blockedList__in=context['blocked'])
+
+				if targetUsername:
+					targetUser = Account.objects.get(user__username=targetUsername)
+					context['targetUser'] = targetUser
+					matches = targetUser.matchHistory.all()
+					context['match'] = {
+						"count": matches.count(),
+						"wins": matches.filter(matchWinnerUsername=targetUser).count(),
+						"losses": matches.filter(matchLoserUsername=targetUser).count()
+					}
+					context['matchData'] = matches.values('matchDate', 'matchWinnerUsername', 'matchLoserUsername', 'matchWinnerScore', 'matchLoserScore')
+
+					if targetUser not in context['blocked']:
+						receivedMessages = user.receivedMessages.filter(messageSender=targetUser).order_by('messageDate')
+						sentMessages = user.sentMessages.filter(messageReceiver=targetUser).order_by('messageDate')
+						context['messages'] = (receivedMessages | sentMessages).order_by('messageDate')
+					else:
+						context['messages'] = None
+				else:
+					matches = user.matchHistory.all()
+					context['match'] = {
+						"count": matches.count(),
+						"wins": matches.filter(matchWinnerUsername=user).count(),
+						"losses": matches.filter(matchLoserUsername=user).count()
+					}
+					context['matchData'] = matches.values('matchDate', 'matchWinnerUsername', 'matchLoserUsername', 'matchWinnerScore', 'matchLoserScore')
+					context['targetUser'] = None
+					context['messages'] = None
 			except:
 				context['user'] = None
+				context['friends'] = None
+				context['blocked'] = None
+				context['targetUser'] = None
+				context['match'] = None
+				context['matchData'] = None
+				context['messages'] = None
 			try:
 				template = loader.get_template('pong/' + content + '.html')
 			except:
