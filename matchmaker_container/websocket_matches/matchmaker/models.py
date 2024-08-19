55376c2444ca514d142fe7208e7ba8fd17ad074f
Linus Broms <linus@MacBook-Pro.lan>
2024-08-19 12:44:39 +0300

    LocalTournament: Add local tournament model

--- matchmaker_container/websocket_matches/matchmaker/models.py
+++ matchmaker_container/websocket_matches/matchmaker/models.py
@@ -22,7 +22,15 @@ class OnlineTournament(models.Model):
 	player2 = models.CharField(max_length=15)
 	player3 = models.CharField(max_length=15)
 	player4 = models.CharField(max_length=15)
+
 class LocalMatch(models.Model):
 	roomId = models.CharField(max_length=36)
 	player1 = models.CharField(max_length=15)
 	player2 = models.CharField(max_length=15)
+
+class LocalTournament(models.Model):
+	roomId = models.CharField(max_length=200)
+	player1 = models.CharField(max_length=15)
+	player2 = models.CharField(max_length=15)
+	player3 = models.CharField(max_length=15)
+	player4 = models.CharField(max_length=15)
