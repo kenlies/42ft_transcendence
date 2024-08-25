c763941c0de6816df573497a7fae41c4a096cbe1
Linus Broms <linus@MacBook-Pro.lan>
2024-08-25 13:35:55 +0300

    CliClient: Change script to just check that requirements are met. If not, let the user know.

--- cli_client.sh
+++ cli_client.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-pip3 install -r ./cli_client/requirements.txt >/dev/null 2>&1
+echo "Checking for required packages..."
+
+if ! hash python3 &>/dev/null; then
+	echo "Python is not installed. Exiting..."
+	exit 1
+fi
+
+if python3 -c 'import sys; sys.version_info.major >= 3 and sys.version_info.minor >= 5' 2>/dev/null; then
+	echo "Python version satisfies the requirements."
+else
+	echo "Python version is not compatible. Exiting..."
+	exit 1
+fi
+
+for p in $(cat cli_client/requirements.txt); do
+	if ! python3 -c "import $p" &>/dev/null; then
+		echo "$p not found. Please install $p to continue. Exiting..." >&2
+		exit 1
+	fi
+done
+echo "All required packages are installed. Starting the CLI client..."
+
 cd cli_client
 
 # Ignore Ctrl+Z (SIGTSTP)
