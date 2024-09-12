ed007a7ba85c100ec6df33e110bb622377bf668c
Linus Broms <linus@MacBook-Pro.lan>
2024-09-12 20:46:33 +0300

    Move token keys to env file

--- flow_container/flow/flow/settings.py
+++ flow_container/flow/flow/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)!7k%m38h4r8p8$*we@-+pe85_lopd)%bj)$6#^rnt=0o@^s3'
+SECRET_KEY = os.environ.get("FLOW_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- matchmaker_container/websocket_matches/websocket_matches/settings.py
+++ matchmaker_container/websocket_matches/websocket_matches/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52is2=p8*jw$&($b&8rtuy#txdr%5f#=i&*he)429xnop=a+8g'
+SECRET_KEY = os.environ.get("MATCH_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
